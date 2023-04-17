a9c3ba328504426bb14fbbf7549fed1943c7f676
Martin Rivas <marrivas@frba.utn.edu.ar>
2023-04-17 15:01:22 -0300

    Subiendo primer tp de digitales 3

---
+++ 1_blinky/Debug/Core/Src/main.d
@@ -0,0 +1,102 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ ../Core/Inc/stm32f1xx_hal_conf.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ ../Drivers/CMSIS/Include/core_cm3.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
+ C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/FreeRTOS.h \
+ C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/FreeRTOSConfig.h \
+ C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/projdefs.h \
+ C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/portable.h \
+ C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/deprecated_definitions.h \
+ C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/portmacro.h \
+ C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/mpu_wrappers.h \
+ C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/task.h \
+ C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/list.h
+
+../Core/Inc/main.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+../Core/Inc/stm32f1xx_hal_conf.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+../Drivers/CMSIS/Include/core_cm3.h:
+
+../Drivers/CMSIS/Include/cmsis_version.h:
+
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+
+C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/FreeRTOS.h:
+
+C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/FreeRTOSConfig.h:
+
+C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/projdefs.h:
+
+C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/portable.h:
+
+C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/deprecated_definitions.h:
+
+C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/portmacro.h:
+
+C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/mpu_wrappers.h:
+
+C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/task.h:
+
+C:/Users/marti/STM32CubeIDE/workspace_1.6.1/freertos/Inc/list.h:
